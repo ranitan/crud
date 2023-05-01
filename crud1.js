daee441ec0b290816dd21af3e38e7d0facd3a2a6
ranitan <ranitanagarajan@gmail.com>
2023-05-01 18:59:53 +0530

    asdf

--- crud1.js
+++ crud1.js
@@ -1,7 +1,12 @@
+//https://javascript.info/xmlhttprequest#http-headers
+// npm install -g json-server
+// json-server --watch db.json in Terminal(command prompt)
 function loadTable() {
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://localhost:3000/project");
     xhttp.send();
+    //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest 
+    //XMLHttpRequest Methods and Properties
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         console.log(this.responseText);
@@ -9,23 +14,23 @@ function loadTable() {
         const objects = JSON.parse(this.responseText);
         for (let object of objects) {
           trHTML += "<tr>";
-          trHTML += "<td>" + object["Projectid"] + "</td>";
-          trHTML += '<td><img width="50px"  src="' +
-          object["Projectlogo"] +
-          '" class="Projectlogo"></td>';
-            
-          trHTML += "<td>" + object["Projectname"] + "</td>";
-          trHTML += "<td>" + object["Leadname"] + "</td>";
-          trHTML += "<td>" + object["Startdate"] +"</td>";
-          trHTML += "<td>" + object["Duration"] + "</td>";
-          trHTML += "<td>" + object["Memberno"] + "</td>";
+          trHTML += "<td>" + object["id"] + "</td>";
+          trHTML +=
+            '<td><img width="50px" src="' +
+            object["projectlogo"] +
+            '" class="projectlogo"></td>';
+            trHTML += "<td>" + object["Projectname"] + "</td>";
+            trHTML += "<td>" + object["Leadname"] + "</td>";
+            trHTML += "<td>" + object["Startdate"] +"</td>";
+            trHTML += "<td>" + object["Duration"] + "</td>";
+            trHTML += "<td>" + object["Memberno"] + "</td>";
           trHTML +=
             '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
-            object["Projectid"] +
+            object["id"] +
             ')">Edit</button>';
           trHTML +=
             '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
-            object["Projectid"] +
+            object["id"] +
             ')">Del</button></td>';
           trHTML += "</tr>";
         }
@@ -36,44 +41,43 @@ function loadTable() {
   
   loadTable();
 
-   function showUserCreateBox() {
+  function showUserCreateBox() {
+    //https://sweetalert2.github.io/v9.html
+    Swal.fire({
+      title: "Create user",
+      html:
+        '<input id="id" type="hidden">' +
+        '<input id="Projectname" class="swal2-input" placeholder="Projectname">' +
+        '<input id="Leadname" class="swal2-input" placeholder="Leadname">' +
+        '<input id="Startdate" class="swal2-input" placeholder="Startdate">' +
+        '<input id="Duration" class="swal2-input" placeholder="Duration">'+
+        '<input id="Memberno" class="swal2-input" placeholder="Memberno">',
+      preConfirm: () => {
+        userCreate();
+      },
+    });
+  }
   
-     Swal.fire({
-       title: "Add project",
-       html:
-         '<input id="Projectid" type="hidden">' +
-         '<input id="Projectname" class="swal2-input" placeholder="Projectname">' +
-         '<input id="Leadname" class="swal2-input" placeholder="Leadname">' +
-         '<input id="Startdate" class="swal2-input" placeholder="Startdate">' +
-         '<input id="Duration" class="swal2-input" placeholder="Duration">'+
-         '<input id="Memberno" class="swal2-input" placeholder="Memberno">',
-        
-       preConfirm: () => {
-         CreateProject();
-       },
-     });
-   }
-  
-   function CreateProject() {
-     const Projectname = document.getElementById("Projectname").value;
+  function userCreate() {
+    const Projectname = document.getElementById("Projectname").value;
      const Leadname = document.getElementById("Leadname").value;
      const Startdate = document.getElementById("Startdate").value;
      const Duration = document.getElementById("Duration").value;
      const Memberno = document.getElementById("Memberno").value;
   
-     const xhttp = new XMLHttpRequest();
-     xhttp.open("POST", "http://localhost:3000/project");
-     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-     xhttp.send(
-       JSON.stringify({
-       Projectlogo: "C:/Users/cgvak/Desktop/crud/asset/images/projectrepeat.png",
-       Projectname : Projectname,
-       Leadname : Leadname,
-       Startdate : Startdate,
-       Duration : Duration,
-       Memberno : Memberno,
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "http://localhost:3000/project/");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(
+      JSON.stringify({
+        Projectlogo: "C:/Users/hp/Desktop/crud/asset/images/projectrepeat.png",
+        Projectname : Projectname,
+        Leadname : Leadname,
+        Startdate : Startdate,
+        Duration : Duration,
+        Memberno : Memberno,
       })
-     );
+    );
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         const objects = JSON.parse(this.responseText);
@@ -83,21 +87,21 @@ function loadTable() {
     };
   }
 
-  function showUserEditBox(Projectid) {
-    console.log(Projectid);
+  function showUserEditBox(id) {
+    console.log(id);
     const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://localhost:3000/project/${Projectid}`);
+    xhttp.open("GET", `http://localhost:3000/project/${id}`);
     xhttp.send();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         const objects = JSON.parse(this.responseText);
-      
+        //const user = objects["objects"];
         console.log(objects);
         Swal.fire({
-          title: "Edit project",
+          title: "Edit User",
           html:
-            '<input id="Projectid" type="hidden" value="' +
-            objects[`${Projectid}`] +
+            '<input id="id" type="hidden" value="' +
+            objects[`${id}`] +
             '">' +
             '<input id="Projectname" class="swal2-input" placeholder="Projectname" value="' +
             objects["Projectname"] +
@@ -115,33 +119,35 @@ function loadTable() {
             objects["Startdate"] +
             '">',
           preConfirm: () => {
-            userEditProject(Projectid);
+            userEdit(id);
           },
         });
       }
     };
   }
   
-  function userEditProject(Projectid) {
+  function userEdit(id) {
+    //const id = document.getElementById("id").value;
     const Projectname = document.getElementById("Projectname").value;
     const Leadname = document.getElementById("Leadname").value;
     const Startdate = document.getElementById("Startdate").value;
     const Duration = document.getElementById("Duration").value;
     const Memberno = document.getElementById("Memberno").value;
 
-    console.log(Projectid);
-
+    console.log(id);
+    console.log(Projectname);
     const xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", `http://localhost:3000/project/${Projectid}`);
+    xhttp.open("PUT", `http://localhost:3000/project/${id}`);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(
       JSON.stringify({
-        Projectlogo: "C:/Users/cgvak/Desktop/crud/asset/images/projectrepeat.png",
-        Projectname : Projectname,
-        Leadname : Leadname,
-        Startdate : Startdate,
-        Duration : Duration,
-        Memberno : Memberno,
+       // id: id,
+       Projectlogo: "C:/Users/cgvak/Desktop/crud/asset/images/projectrepeat.png",
+       Projectname : Projectname,
+       Leadname : Leadname,
+       Startdate : Startdate,
+       Duration : Duration,
+       Memberno : Memberno,
       })
     );
     xhttp.onreadystatechange = function () {
@@ -153,14 +159,14 @@ function loadTable() {
     };
   }
 
-  function userDeleteProject(Projectid) {
-    console.log(Projectid);
+  function userDelete(id) {
+    console.log(id);
     const xhttp = new XMLHttpRequest();
-    xhttp.open(`DELETE`, `http://localhost:3000/project/${Projectid}`);
+    xhttp.open(`DELETE`, `http://localhost:3000/project/${id}`);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(
       JSON.stringify({
-        Projectid: Projectid,
+        id: id,
       })
     );
     xhttp.onreadystatechange = function () {
@@ -181,6 +187,6 @@ function loadTable() {
           }
       })
       }
-      loadTable();
+      //loadTable();
     };
   }
